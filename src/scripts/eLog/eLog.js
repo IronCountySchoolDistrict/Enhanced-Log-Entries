884dc4ba9b5f4067c48d006a8124f7f48c0e24f7
jonesnc <purkinje90@gmail.com>
2017-01-04 13:52:11 -0700

    Use es2015 module syntax, code reformatting

--- src/scripts/eLog/eLog.js
+++ src/scripts/eLog/eLog.js
@@ -1,30 +1,28 @@
-define(['angular', 'components/shared/index'], function(angular) {
+import angular from 'angular';
+import * as index from 'components/shared/index';
 
-  var eLogApp = angular.module('eLogMod', ['powerSchoolModule']);
+var eLogApp = angular.module('eLogMod', ['powerSchoolModule']);
 
-  eLogApp.controller('eLogCtrl', function($rootScope, $scope, $compile, eLogGetService, i18nUtils) {
-    loadingDialog();
+eLogApp.controller('eLogCtrl', function($rootScope, $scope, $compile, eLogGetService, i18nUtils) {
+  loadingDialog();
 
-    $scope.eLogList = [];
+  $scope.eLogList = [];
 
-    eLogGetService.geteLog('eLog-roles.json').then(function(retData) {
-      for (var i = 0; i < retData.length; i++) {
-        retData[i].DATE = i18nUtils.parseLocalizedDate(retData[i].DATE);
-      }
-      $scope.eLogList = retData;
-      closeLoading();
-    });
-  });
-
-  eLogApp.factory('eLogGetService', function($http) {
-    return {
-      geteLog: function(datafile) {
-        return $http.get(datafile)
-          .then(function(result) {
-            return result.data;
-          });
-      }
+  eLogGetService.geteLog('eLog-roles.json').then(function(retData) {
+    for (var i = 0; i < retData.length; i++) {
+      retData[i].DATE = i18nUtils.parseLocalizedDate(retData[i].DATE);
     }
+    $scope.eLogList = retData;
+    closeLoading();
   });
+});
 
+eLogApp.factory('eLogGetService', function($http) {
+  return {
+    geteLog: function(datafile) {
+      return $http.get(datafile).then(function(result) {
+        return result.data;
+      });
+    }
+  }
 });
